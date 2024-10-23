e1e02ea7a0f84bfa834110e9a2b89ef4cfc09f50
akidon0000 <akidon0000@gmail.com>
2024-10-23 23:31:36 +0900

    add isimcameratool.rb

---
+++ isimcameratool.rb
@@ -0,0 +1,15 @@
+class Isimcameratool < Formula
+  desc "IPhoneシミュレーターにMacのカメラを表示させるためのMacアプリです"
+  homepage "https://github.com/akidon0000/isimcameratool"
+  url "https://github.com/akidon0000/iSimCameraTool/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "565483b1e1c26c36c4f74595ba3766b58769f1a71682b5ecd5c508768fae556f"
+  license "MIT"
+
+  def install
+    system "./configure", "--disable-silent-rules", *std_configure_args
+  end
+
+  test do
+    system "false"
+  end
+end
